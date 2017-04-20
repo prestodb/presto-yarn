prestodb/presto-yarn

commit 1d745ea30534acfb23a343b023f48ffb9b8c6c41

Allow log properties configurations  (#66)
Allow log properties to be configured

presto-yarn-package/src/main/slider/package/scripts/configure.py
@@ -47,6 +47,9 @@ def set_configuration(component=None):
     if params.jvm_args:
         _parse_array_and_write(params.jvm_args, format("{params.conf_dir}/jvm.config"))
 
+    if params.jvm_args:
+        _parse_array_and_write(params.log_properties, format("{params.conf_dir}/log.properties"))
+
     if params.additional_config_properties:        
         _parse_array_and_write(params.additional_config_properties, format("{params.conf_dir}/config.properties"))
 

presto-yarn-package/src/main/slider/package/scripts/params.py
@@ -52,6 +52,7 @@ presto_query_max_memory = config['configurations']['global']['presto_query_max_m
 presto_query_max_memory_per_node = config['configurations']['global']['presto_query_max_memory_per_node']
 presto_server_port = config['configurations']['global']['presto_server_port']
 jvm_args = default('/configurations/global/jvm_args', '')
+log_properties = default('/configurations/global/log_properties', '')
 
 node_id = uuid.uuid1()
 

commit 3b54d13d067cf211531476454342bfb969de2cd9

Correct a bug introduced in https://github.com/prestodb/presto-yarn/pull/66/

presto-yarn-package/src/main/slider/package/scripts/configure.py
@@ -47,7 +47,7 @@ def set_configuration(component=None):
     if params.jvm_args:
         _parse_array_and_write(params.jvm_args, format("{params.conf_dir}/jvm.config"))
 
-    if params.jvm_args:
+    if params.log_properties:
         _parse_array_and_write(params.log_properties, format("{params.conf_dir}/log.properties"))
 
     if params.additional_config_properties:        